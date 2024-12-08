b69ddf03a17ea86d0854be3d5ce3c99ff4597132
amine-1905 <amineberraki22@gmail.com>
2024-12-08 06:46:53 +0100

    Update JeuDeLaVie.h

--- JeuDeLaVie.h
+++ JeuDeLaVie.h
@@ -2,18 +2,28 @@
 #define JEUDELAVIE_H
 
 #include "Grille.h"
+#include "Fichier.h"
+#include "InterfaceGraphique.h"
 #include <string>
+
+enum class ModeAffichage {
+    CONSOLE,
+    GRAPHIQUE
+};
 
 class JeuDeLaVie {
 private:
     Grille grille;
-    int iterations;
+    int maxIterations;
+    ModeAffichage mode;
+    float iterationTime; // Temps entre itérations en mode graphique
 
 public:
-    JeuDeLaVie(const std::string& cheminFichier);
-    void demarrerSimulation();
-    void chargerEtatInitial(const std::string& cheminFichier);
-    void sauvegarderEtat(const std::string& cheminFichier);
+    JeuDeLaVie(const std::string& fichierEntree, int iterations);
+    JeuDeLaVie(int hauteur, int largeur, float iterationTime);
+
+    void executerConsole(const std::string& fichierSortie);
+    void demarrerSimulationGraphique();
 };
 
 #endif // JEUDELAVIE_H
